d13eee56a8583105c50e9052dd8c749bc7a0fc75
RenyJoseph28 <renyjoseph28@gmail.com>
2022-05-18 06:33:39 +0530

    productapp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import ProductApp from './component/ProductApp';
+import Search from './component/Search';
 
 function App() {
   return (
     <div>
       <ProductApp/>
+      <Search/>
     </div>
   );
 }
--- src/component/ProductApp.js
+++ src/component/ProductApp.js
@@ -3,24 +3,24 @@ import React from 'react'
 const ProductApp = () => {
   return (
     <div>
-<div class="container">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row">
-                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <label for="" class="form-label">Product Name</label>
-                <input type="text" class="form-control"/>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-2">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label for="" className="form-label">Product Name</label>
+                <input type="text" className="form-control"/>
                 </div>
-                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label for="" class="form-label">Price</label>
-                    <input type="text" class="form-control"/>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Price</label>
+                    <input type="text" className="form-control"/>
                 </div>
-                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label for="" class="form-label">Description</label>
-                    <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Description</label>
+                    <textarea className="form-control" name="" id="" cols="30" rows="10"></textarea>
                 </div>
-                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button class="btn btn-success">Submit</button>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">Submit</button>
                 </div>
             </div>
 
